99804252bea2b1ae606ca545fa07ef80ed5fc0f5
pablo <pablo@gmail.com>
2022-11-28 13:26:04 +0100

    asd

---
+++ app/src/main/java/com/deneb/asteroides/Juego.java
@@ -0,0 +1,14 @@
+package com.deneb.asteroides;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Juego extends AppCompatActivity {
+
+    @Override
+    public void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.juego);
+    }
+}
---
+++ app/src/main/java/com/deneb/asteroides/VistaJuego.java
@@ -0,0 +1,22 @@
+package com.deneb.asteroides;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+
+public class VistaJuego extends View {
+    private List<Grafico> asteroides;
+    private int numAsteroides = 5;
+    private int numFragmentos = 3;
+
+    public VistaJuego(Context context, AttributeSet attrs){
+        super(context, attrs);
+        Drawable drwawbleNave, drawableAsteroide, drawableMisil;
+        drawableAsteroide = AppCompatActivity
+    }
+}
